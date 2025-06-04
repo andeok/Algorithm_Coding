876b405a3805527554765ea47e6ffb9910e16d59
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-06-04 15:38:38 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+class Main {
+	public static void main(String[] args) {
+		String a1 = "         ,r'\"7";
+		String a2 = "r`-_   ,'  ,/";
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+}
