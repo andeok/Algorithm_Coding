df7644851f9b0cb06c88b00aff07be1da2223ea3
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-06-04 15:39:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, result;
+
+		a = sc.nextInt();
+		result = a - 543;
+		System.out.print(result);
+	}
+}
