eec09763cc7724fd4ff81efdafada6ebc422f6bc
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-06-04 15:40:31 +0900

    [Bronze V] Title: 사칙연산, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String n = sc.nextLine();
+
+    String[] s = n.split("\\s");
+
+    int a = Integer.parseInt(s[0]);
+    int b = Integer.parseInt(s[1]);
+
+    System.out.println(a + b);
+    System.out.println(a - b);
+    System.out.println(a * b);
+    System.out.println(a / b);
+    System.out.println(a % b);
+
+  }
+}
