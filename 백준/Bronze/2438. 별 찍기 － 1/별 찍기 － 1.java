e6aed74c5cb675a8db9b78ab922cf2756b37dde7
Deokgyun an <97350241+andeok@users.noreply.github.com>
2025-11-11 16:09:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br;
+	static BufferedWriter bw;
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+		new Main().solution();
+	}
+
+	public void solution() throws Exception {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		//br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_102_루프/input.txt")));
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < n; i++) {
+			String star = "*".repeat(i + 1);
+			bw.write(star + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
